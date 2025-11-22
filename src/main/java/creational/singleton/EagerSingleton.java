64cadb157fd4c3006a116b979ef433606a67cd60
G LEELA SIVA SAI KUMAR <glssaikumar.2697@gmail.com>
2025-11-22 18:58:48 +0530

    Singleton Pattern, EagerSingleton, LazySingletonThreadUnsafe, LazySingletonThreadSafe

---
+++ src/main/java/creational/singleton/EagerSingleton.java
@@ -0,0 +1,13 @@
+package creational.singleton;
+
+public class EagerSingleton {
+    private static final EagerSingleton instance = new EagerSingleton();
+
+    private EagerSingleton() {
+        // private constructor to prevent instantiation
+    }
+
+    public static EagerSingleton getInstance() {
+        return instance;
+    }
+}
